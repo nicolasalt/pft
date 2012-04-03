973d32fcf20d54756f422e43666c65416d5d16d6
Nikolay Zherebtsov <nicolas.zherebtsov@gmail.com>
2012-04-03 23:16:16 +0200

    Added transactions.

--- datastore/models.py
+++ datastore/models.py
@@ -9,9 +9,16 @@ class UserSettings(db.Model):
   def MakeKey(user_id):
     return db.Key.from_path('User', user_id)
 
+
 class Account(db.Model):
   owner = db.UserProperty(required=True)
   name = db.StringProperty(required=True)
   currency = db.StringProperty(required=True)
   balance = db.FloatProperty(required=True, default=0.0)
   date = db.DateTimeProperty(required=True, auto_now_add=True)
+
+
+class Transaction(db.Model):
+  account_id = db.IntegerProperty(required=True)
+  amount = db.FloatProperty(required=True)
+  date = db.DateTimeProperty(required=True)
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import cgi
 import datetime
-import urllib
 import webapp2
 import jinja2
 import os
@@ -21,11 +19,17 @@ class MainPage(webapp2.RequestHandler):
 
     user_settings = lookup.GetOrCreateUserSettings(visitor)
 
-    query = models.Account.all().ancestor(user_settings).order('-date')
-    accounts = query.fetch(100)
+    account_query = models.Account.all().ancestor(user_settings).order('-date')
+    accounts = account_query.fetch(100)
+
+    transaction_query = models.Transaction.all().ancestor(
+        user_settings).order('-date')
+    transactions = transaction_query.fetch(100)
+#    transaction_dict = dict([(t.account, t) for t in transactions])
 
     template_values = {
       'accounts': accounts,
+      'transactions': transactions
     }
 
     template = jinja_environment.get_template('templates/index.html')
@@ -49,7 +53,42 @@ class DoAddAccount(webapp2.RequestHandler):
     self.redirect('/')
 
 
+def transaction(func):
+  def _transaction(*args, **kwargs):
+    db.run_in_transaction(func, *args, **kwargs)
+  return _transaction
+
+
+@transaction
+def AddTransaction(user_settings, account_id, amount, date):
+  account = models.Account.get_by_id(account_id, parent=user_settings.key())
+  transaction = models.Transaction(
+    parent=user_settings.key(),
+    account_id=account.key().id(),
+    amount=amount,
+    date=date).put()
+  account.balance -= amount
+  account.put()
+
+
+class DoAddTransaction(webapp2.RequestHandler):
+  def post(self):
+    visitor = users.get_current_user()
+    if not visitor:
+      self.redirect(users.create_login_url(self.request.uri))
+
+    user_settings = lookup.GetOrCreateUserSettings(visitor)
+    amount = float(self.request.get('amount'))
+    account_id = int(self.request.get('account_id'))
+
+    AddTransaction(user_settings, account_id,
+                   amount, datetime.datetime.now())
+
+    self.redirect('/')
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/do/add_account', DoAddAccount)],
+    ('/do/add_account', DoAddAccount),
+    ('/do/add_transaction', DoAddTransaction)],
     debug=True)

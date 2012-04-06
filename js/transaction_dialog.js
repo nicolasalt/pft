abe4859603f24a2415cac27c58b5fe93c5a55e25
Nikolay Zherebtsov <nicolas.zherebtsov@gmail.com>
2012-04-06 17:12:38 +0200

    none

--- js/transaction_dialog.js
+++ js/transaction_dialog.js
@@ -4,17 +4,31 @@ pf.transactionDialog.open = function(transaction_id) {
   var transaction = pf.state.GetTransaction(transaction_id);
   if (!transaction) return;
 
-  $('#transaction-dialog-amount').val(transaction['amount']);
   $('#transaction-dialog-description').val(transaction['description']);
+  $('#transaction-dialog-datepicker').datepicker('setDate', transaction['date']);
+  $('#transaction-dialog-category-' + transaction['category_id']).attr('checked', true);
+  $('#transaction-dialog-amount').val(transaction['amount']);
+  $('#transaction-dialog').dialog('option', 'title', transaction['date']);
   $('#transaction-dialog').dialog('open');
+  $('#transaction-dialog-amount').focus();
 };
 
 $(function() {
   $('#transaction-dialog').dialog({
-    autoOpen: false
+    autoOpen: false,
+    resizable: false,
+    buttons: {
+      'Save': function() {
+        $(this).dialog('close');
+      },
+      'Cancel': function() {
+        $(this).dialog('close');
+      }
+    }
   });
   $('#transaction-dialog-datepicker').datepicker({
-    inline: true
+    inline: true,
+    dateFormat: 'dd.mm.yy'
   });
   $('#transaction-dialog-categories').buttonset();
 

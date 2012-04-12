e22c2c19777ca97fa28d7cda6888bcd37b994393
Nikolay Zherebtsov <nicolas.zherebtsov@gmail.com>
2012-04-12 23:32:46 +0200

    Added chart for savings/expenses.

--- view_handlers.py
+++ view_handlers.py
@@ -22,26 +22,30 @@ class MainPage(CommonHandler):
     categories_total_balance = sum([c.balance for c in self.profile.categories])
 
     budget_view_items = []
-    common_savings = 0
+    common_savings = 9000
+    max_expense = 0
     for item in budget.items:
-      if item.category_id:
+      if item.category_id is not None:
         budget_view_items.append({
           'name': self.profile.categories[item.category_id].name,
           'amount': item.planned_amount
         })
         common_savings -= item.planned_amount
+        max_expense = max(max_expense, item.planned_amount)
 
     budget_view_items.append({
       'name': 'Other expenses',
       'amount': unplanned_expenses
     })
     common_savings -= unplanned_expenses
+    max_expense = max(max_expense, item.planned_amount)
 
     template_values = {
       'total_balance': total_balance,
       'categories_total_balance': categories_total_balance,
       'budget_items': budget_view_items,
-      'common_savings': common_savings
+      'common_savings': common_savings,
+      'max_expense': max_expense
     }
 
     self.WriteToTemplate('templates/index.html', template_values)

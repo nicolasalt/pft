d0936241392def326370f06c8d57cd3f153e4e0c
Nikolay Zherebtsov <nicolas.zherebtsov@gmail.com>
2012-06-20 00:49:28 +0200

    Ported account widget and categories widget to angular (width dialogs).

--- js/app_ctrl.js
+++ js/app_ctrl.js
@@ -2,13 +2,118 @@
 pft.mainModule = angular.module('pftApp', ['ngResource', 'pft.directives']);
 
 pft.AppCtrl = function($scope, $resource) {
-  $scope.name = 'Nikolay';
-
-  var api = $resource('/api/get_profile', {}, {
+  var getProfileRequest = $resource('/api/get_profile', {}, {
     query: {method:'GET'}
   });
 
-  $scope.profileData = api.get(null, function() {
-    $scope.profileDataFormatted = angular.toJson(eval($scope.profileData), true);
-  });
+  $scope.profile = null;
+
+  $scope.updateProfile = function () {
+    $scope.profileData = getProfileRequest.get(null, function() {
+      $scope.profileDataFormatted = angular.toJson(eval($scope.profileData), true);
+      $scope.profile = $scope.profileData.profile;
+    });
+  };
+
+  $scope.updateProfile();
+
+  $scope.openEditCategoryDialog = function(opt_catId) {
+    var category = $scope.profile.categories[opt_catId];
+
+    var title = 'Edit category';
+    if (opt_catId == undefined) {
+      category = {
+        'name': '',
+        'balance': ''
+      };
+      title = 'Add new category';
+    }
+    $('#edit-category-name').val(category['name']);
+    $('#edit-category-balance').val(category['balance']);
+    $('#edit-category-dialog').dialog({
+        resizable: false,
+        title: title,
+        buttons: {
+          'Save': function() {
+            $scope.saveCategory(opt_catId);
+          },
+          'Cancel': function() {
+            $(this).dialog('close');
+          }
+        }
+    });
+  };
+
+  $scope.saveCategory = function(categoryId, opt_delete){
+    var data = {
+      'name': $('#edit-category-name').val()
+    };
+    var balance = $('#edit-category-balance').val();
+    if (balance) {
+      data['balance'] = balance;
+    }
+    if (categoryId != null) {
+      data['category_id'] = categoryId;
+    }
+    if (opt_delete) {
+      data['delete'] = '1';
+    }
+
+    $.post('/do/edit_category', data).success(function() {
+      $scope.updateProfile();
+    });
+    $('#edit-category-dialog').dialog('close');
+  };
+
+  $scope.openEditAccountDialog = function(opt_accountId) {
+    var account = $scope.profile.accounts[opt_accountId];
+
+    var title = 'Edit account';
+    if (opt_accountId == undefined) {
+      account = {
+        'name': '',
+        'balance': '',
+        'currency': 'USD'
+      };
+      title = 'Add new account';
+    }
+    $('#edit-account-name').val(account['name']);
+    $('#edit-account-balance').val(account['balance']);
+    $('#edit-account-currency').val(account['currency']);
+    $('#edit-account-dialog').dialog({
+        resizable: false,
+        title: title,
+        buttons: {
+          'Save': function() {
+            $scope.saveAccount(opt_accountId);
+          },
+          'Cancel': function() {
+            $(this).dialog('close');
+          }
+        }
+    });
+  };
+
+
+  $scope.saveAccount = function(opt_accountId, opt_delete){
+    var data = {
+      'name': $('#edit-account-name').val(),
+      'currency': $('#edit-account-currency').val()
+    };
+    var balance = $('#edit-account-balance').val();
+    if (balance) {
+      data['balance'] = balance;
+    }
+    if (opt_accountId != null) {
+      data['account_id'] = opt_accountId;
+    }
+    if (opt_delete) {
+      data['delete'] = '1';
+    }
+
+    $.post('/do/edit_account', data).success(function() {
+      $scope.updateProfile();
+    });
+    $('#edit-account-dialog').dialog('close');
+  };
 };

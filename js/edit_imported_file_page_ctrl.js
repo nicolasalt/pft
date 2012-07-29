a3c5bfec0b0deef290f3e2aa95c3c52473899c94
Nikolay Zherebtsov <nicolas.zherebtsov@gmail.com>
2012-07-29 23:01:27 +0200

    Porting import file - accepting transactions (only single categories).

--- js/edit_imported_file_page_ctrl.js
+++ js/edit_imported_file_page_ctrl.js
@@ -33,4 +33,54 @@ pft.EditImportedFilePageCtrl = function($scope, $resource, $routeParams, $http) 
           'schema': $scope.schemaToAdd}).success(
         $scope.updateProfile);
   };
+
+
+  $scope.currentTransactionIndex = 0;
+  $scope.getCurrentTransaction = function() {
+    if (!$scope.importedFile || !$scope.importedFile.parsed_transactions) return null;
+
+    return $scope.importedFile.parsed_transactions[$scope.currentTransactionIndex];
+  };
+  $scope.nextTransaction = function () {
+    if (!$scope.importedFile || !$scope.importedFile.parsed_transactions) return;
+
+    if ($scope.currentTransactionIndex < $scope.importedFile.parsed_transactions.length - 1) {
+      $scope.currentTransactionIndex++;
+    }
+  };
+  $scope.previousTransaction = function () {
+    if (!$scope.importedFile || !$scope.importedFile.parsed_transactions) return;
+
+    if ($scope.currentTransactionIndex > 0) {
+      $scope.currentTransactionIndex--;
+    }
+  };
+  $scope.switchToTransaction = function (tIndex) {
+    $scope.currentTransactionIndex = tIndex;
+  };
+  $scope.resolveTransaction = function (category, opt_drop) {
+    var catToAmount = {};
+    catToAmount[category.category_id] = $scope.getCurrentTransaction().amount;
+
+    var data = {
+      'imported_file_id': $scope.importedFile.id,
+      'transaction_index': $scope.currentTransactionIndex};
+
+    if (opt_drop) {
+      data['drop'] = '1';
+    } else {
+      var categoriesToSend = [];
+      var amountsToSend = [];
+      for (var catId in catToAmount) {
+        categoriesToSend.push(catId);
+        amountsToSend.push(catToAmount[catId]);
+      }
+
+      data['categories'] = categoriesToSend.join(',');
+      data['amounts'] = amountsToSend.join(',');
+    }
+
+    $.post('/do/resolve_parsed_transaction', data);
+    // TODO: update view.
+  };
 };
